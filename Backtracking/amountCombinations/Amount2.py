fc73c99882d61d7ca24151bf145f5b41ebea1060
Christopher Grigsby <grigsby.c@outlook.com>
2022-07-17 17:25:10 -0400

    feat(amountCombinations): add second approach (#16)

---
+++ Backtracking/amountCombinations/Amount2.py
@@ -0,0 +1,30 @@
+from copy import deepcopy
+
+def amount_helper(numArr, start, result, remainder, combination):
+  if remainder == 0: 
+    if combination not in result: result.append(deepcopy(combination))
+    return
+  if remainder < 0: return 
+  
+  # form the the recurssion tree
+  for i in range(start, len(numArr)):
+      combination.append(numArr[i])
+      amount_helper(numArr, i+1, result, remainder - numArr[i], combination)
+      combination.pop()
+
+def amount(A, S):
+  A.sort()
+  numArr = A
+  print(numArr)
+  targetSum = S
+  start = 0
+  result = []
+  combination = []
+  amount_helper(numArr, start, result, targetSum, combination)
+  return result
+
+if __name__ == "__main__":
+  A = [11,1,3,2,6,1,5]
+  S = 8
+  result = amount(A, S)
+  print("The possible unique combinations from the given amount is:", result)
